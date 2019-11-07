52f9b42b346cd51c453e4231278c8310bbc9870f
mukulrawat1986 <mukulsrawat@gmail.com>
2019-11-07 21:37:21 +0530

    write test for empty string as argument for Hello function
    The next requirement is when our function is called with an empty string it
    should default to prining 'Hello, World'. Write test for this
    requiirement. Refactor the Hell function to test the argument for empty
    string and if its empty change it to 'World'.

--- hello/main.go
+++ hello/main.go
@@ -9,6 +9,9 @@ const (
 // Hello function takes as argument a name and returns a greeting
 // for that person
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return  englishHelloPrefix + name
 }
 
