9bc7741880086a4b486caff49ec75c46f468db23
mukulrawat1986 <mukulsrawat@gmail.com>
2019-11-07 22:09:24 +0530

    refactor and divide code in separate function

--- hello/main.go
+++ hello/main.go
@@ -17,14 +17,19 @@ func Hello(name, language string) string {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string){
+
+	prefix = englishHelloPrefix
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
 	}
-	return prefix + name
+	return
 }
 
 func main() {
