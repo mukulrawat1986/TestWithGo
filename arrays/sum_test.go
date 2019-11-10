41848a97f03b9d3505c0663187f632be956c3e50
mukulrawat1986 <mukulsrawat@gmail.com>
2019-11-10 16:10:50 +0530

    refactor and move our assertions to a separate function

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -4,15 +4,19 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
+	assertSum := func(t *testing.T, got, want int, numbers []int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %d want %d, given %v", got, want, numbers)
+		}
+	}
+
 	t.Run("collection of 5 numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
 		want := 15
-
-		if got != want {
-			t.Errorf("got %d want %d, given %v", got, want, numbers)
-		}
+		assertSum(t, got, want, numbers)
 	})
 
 	t.Run("collection of any size", func(t *testing.T) {
@@ -20,10 +24,6 @@ func TestSum(t *testing.T) {
 
 		got := Sum(numbers)
 		want := 6
-
-		if got != want {
-			t.Errorf("got %d want %d, given %v", got, want, numbers)
-		}
-
+		assertSum(t, got, want, numbers)
 	})
 }
