27815eb1de9e51e41707659f771cc5928a96555b
mukulrawat1986 <mukulsrawat@gmail.com>
2019-11-17 16:32:32 +0530

    refactor and move our slice comparison into a separate function

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -35,9 +35,7 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+	checkSums(t, got, want)
 }
 
 func TestSumAllTails(t *testing.T) {
@@ -46,9 +44,7 @@ func TestSumAllTails(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 
 	})
 
@@ -56,8 +52,13 @@ func TestSumAllTails(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v, want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
+}
+
+func checkSums(t *testing.T, got, want []int) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
 }
