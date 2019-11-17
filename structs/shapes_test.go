180afa720691ad5b47ed8680ecd49c6cf24ad664
mukulrawat1986 <mukulsrawat@gmail.com>
2019-11-17 17:05:47 +0530

    write structs for type Rectangle and Circle, write the method and test them

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -2,20 +2,31 @@ package main
 
 import "testing"
 
-func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
-	want := 40.0
-
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
-	}
-}
-
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.0
+	t.Run("rectangle", func(t *testing.T) {
+		r := Rectangle{
+			Width:  10.0,
+			Height: 10.0,
+		}
 
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
-	}
+		got := r.Area()
+		want := 100.0
+
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		c := Circle{
+			Radius: 10.0,
+		}
+
+		got := c.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
