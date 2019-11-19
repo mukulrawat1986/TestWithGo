98d8996182bf0265b2f85caf3ecce13643a09743
mukulrawat1986 <mukulsrawat@gmail.com>
2019-11-19 14:08:17 +0530

    add name for each test case

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -4,10 +4,12 @@ import "testing"
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
 		{
+			"Rectangle",
 			Rectangle{
 				12,
 				6,
@@ -15,12 +17,14 @@ func TestArea(t *testing.T) {
 			72.0,
 		},
 		{
+			"Circle",
 			Circle{
 				10.0,
 			},
 			314.1592653589793,
 		},
 		{
+			"Triangle",
 			Triangle{
 				12.0,
 				6.0,
@@ -30,11 +34,14 @@ func TestArea(t *testing.T) {
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		want := tt.want
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			want := tt.want
 
-		if got != want {
-			t.Errorf("got %g, want %g", got, want)
-		}
+			if got != want {
+				t.Errorf("%#v got %g, want %g", tt.shape, got, want)
+			}
+		})
+
 	}
 }
