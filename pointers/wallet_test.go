df61a8effd0313402efaa31db41cb0e330f0c1db
mukulrawat1986 <mukulsrawat@gmail.com>
2019-11-19 14:55:39 +0530

    write test for the withdraw function

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -17,4 +17,18 @@ func TestWallet(t *testing.T) {
 		}
 	})
 
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{
+			balance: Bitcoin(20),
+		}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
