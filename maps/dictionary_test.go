21ca07cb2cd7721f0bd485692dbfb319a2650330
mukulrawat1986 <mukulsrawat@gmail.com>
2019-11-20 13:51:37 +0530

    refactor and move our assertions to a separate function

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -9,8 +9,13 @@ func TestSearch(t *testing.T) {
 
 	got := Search(dictionary, "test")
 	want := "this is just a test"
+	assertString(t, got, want)
+}
+
+func assertString(t *testing.T, got, want string) {
+	t.Helper()
 
 	if got != want {
-		t.Errorf("got %q want %q, given %q", got, want, "test")
+		t.Errorf("got %q want %q", got, want)
 	}
 }
