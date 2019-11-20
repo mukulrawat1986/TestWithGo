2554ce0fde8935990327ec3c4da28784dc9c6de9
mukulrawat1986 <mukulsrawat@gmail.com>
2019-11-20 14:04:05 +0530

    use the ok property of map lookup and if ok value returned is false then return an error

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,11 +1,17 @@
 package main
 
+import "errors"
+
 // Dictionary type based on the map[string]string type
 type Dictionary map[string]string
 
 // Search method to find the definition of a word in the dictionary
 func (d Dictionary) Search(word string) (string, error) {
-	return d[word], nil
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return definition, nil
 }
 
 func main() {}
