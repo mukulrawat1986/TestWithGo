586e2a14e8a764805b66ed909e4f41b094bb63ad
mukulrawat1986 <mukulsrawat@gmail.com>
2019-11-20 13:49:44 +0530

    declare the Search function

---
+++ maps/dictionary.go
@@ -0,0 +1,8 @@
+package main
+
+// Search function to find the definition of a word in the dictionary
+func Search(dictionary map[string]string, word string) string {
+	return ""
+}
+
+func main() {}
