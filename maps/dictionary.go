df10eee4a38c9c9cf114d0bdd3e02de6ec4ea96c
mukulrawat1986 <mukulsrawat@gmail.com>
2019-11-20 13:50:15 +0530

    write minimal code and pass the test

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -2,7 +2,7 @@ package main
 
 // Search function to find the definition of a word in the dictionary
 func Search(dictionary map[string]string, word string) string {
-	return ""
+	return dictionary[word]
 }
 
 func main() {}
