9ab0816afe42a0e390a46045cf303581b03f4d03
mukulrawat1986 <mukulsrawat@gmail.com>
2019-11-21 16:36:21 +0530

    write test for the case when the Updated word does not exist in the dictionary

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -64,6 +64,15 @@ func TestUpdate(t *testing.T) {
 
 	})
 
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
+	})
 }
 
 func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
