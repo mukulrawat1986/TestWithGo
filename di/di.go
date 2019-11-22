a5f11038c50a03648c2f9c2a5fdab0da3ff178b2
mukulrawat1986 <mukulsrawat@gmail.com>
2019-11-22 14:08:08 +0530

    implement the Greet function and pass the test

--- di/di.go
+++ di/di.go
@@ -1,10 +1,13 @@
 package main
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 // Greet function greets a person
 func Greet(w io.Writer, name string) {
-
+	fmt.Fprintf(w, "Hello, %s", name)
 }
 
 func main() {}
