f1670b02bb87aaf7fd516a4a6e582a946fa9c5dd
mukulrawat1986 <mukulsrawat@gmail.com>
2019-11-22 14:19:23 +0530

    write minimal code and pass the test

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
 )
 
 // Countdown function prints out the countdown on the commandline
-func Countdown(w io.Writer) {}
+func Countdown(w io.Writer) {
+	fmt.Fprintf(w, "3")
+}
 
 func main() {
 	Countdown(os.Stdout)
