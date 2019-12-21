e7d4de112f42d46acc636f4ce476663edf69a6b1
mukulrawat1986 <mukulsrawat@gmail.com>
2019-12-21 12:04:33 +0530

    use channels and solve the race condition

--- concurrency/checkwebsites.go
+++ concurrency/checkwebsites.go
@@ -1,19 +1,29 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
 		go func(url string) {
-			results[url] = wc(url)
+			resultChannel <- result{
+				url,
+				wc(url),
+			}
 		}(url)
 	}
 
-	time.Sleep(2 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
 
 	return results
 }
