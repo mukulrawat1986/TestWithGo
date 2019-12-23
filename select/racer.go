b94cd63e080bedafb5c7ea2499c643bef24b0083
mukulrawat1986 <mukulsrawat@gmail.com>
2019-12-23 19:57:00 +0530

    implement the racer function and pass the test

--- select/racer.go
+++ select/racer.go
@@ -1,7 +1,24 @@
 package main
 
+import (
+	"net/http"
+	"time"
+)
+
 func Racer(a, b string) string {
-	return ""
+	startA := time.Now()
+	http.Get(a)
+	aDuration := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	bDuration := time.Since(startB)
+
+	if aDuration < bDuration {
+		return a
+	}
+
+	return b
 }
 
 func main() {}
