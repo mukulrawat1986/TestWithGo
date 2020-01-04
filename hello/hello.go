b761c9e0d6a3b713188a33ddb4eada62b588a8c9
mukulrawat1986 <mukulsrawat@gmail.com>
2020-01-04 16:08:52 +0530

    separate concern and make our function testable

--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// Hello returns a string greeting
+func Hello() string {
+	return "Hello, World"
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(Hello())
 }
